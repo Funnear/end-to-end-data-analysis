c1d549500f4be25bb74bcc9ebf6090d4b53156aa
Faniia Prazdnikova <funnear@gmail.com>
2025-03-04 17:36:54 +0100

    added ideas to follow up on dataset maintenance

--- src/datasets.py
+++ src/datasets.py
@@ -2,8 +2,14 @@ import pandas as pd
 
 # IDEAS
 
-# path3="../datasets/dataset-of-songs-in-spotify/genres_v2.csv"
+# repeated chunk of code for start of EDA:
 
+# path3="../datasets/dataset-of-songs-in-spotify/genres_v2.csv"
 # data_3 = pd.read_csv(path3)
 # df3 = data_3.copy()
-# df3.sample(10)
+# df3.head(10)
+
+#  information like source path and data frame variable name can be stored in dictionary in the scope of one project
+
+# add maintenance code for saving data dumps as pickles and jsons using paths and file names from dictionary
+
