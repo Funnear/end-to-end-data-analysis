f84e187365847c98b3c1967170adcb88e5d49114
Faniia Prazdnikova <funnear@gmail.com>
2025-03-05 12:58:36 +0100

    Started automating EDA for clean up purposes.

--- src/datasets.py
+++ src/datasets.py
@@ -63,7 +63,7 @@ class DatasetPack:
     def backup_pack(self):
         backup_dictionary = dict()
         for key, value in self.dictionary.items():
-            backup_dictionary[key] = value.label
+            backup_dictionary[key] = value.source
 
         with open(PACK_BACKUP, "w") as json_file:
             json.dump(backup_dictionary, json_file, indent=4)
@@ -76,7 +76,7 @@ class DatasetPack:
             self.dictionary = json.load(json_file)
         
         for key, value in self.dictionary.items():
-            self.dictionary[key] = Dataset(value)
+            self.dictionary[key] = Dataset(key)
 
         
 
--- src/eda.py
+++ src/eda.py
@@ -1,8 +1,46 @@
 import pandas as pd
+import logging
+from src.datasets import Dataset
 
-def numerical_data_columns(df: pd.DataFrame) -> list:
-    """"
-    This is a test method, it does nothing valuable yet.
-    """
-    
-    return df.describe().columns.tolist()
+class Auto_EDA:
+    def __init__(self, dataset: Dataset):
+        # not df.copy() because this way df can be changes outside of this class
+        self.dataset = dataset
+        self.df = dataset.dataframe
+        self.numerical_columns = self.df.describe().columns.tolist()
+        self.categorical_columns = self.df.select_dtypes(exclude="number").columns.tolist()
+
+    def numerical_columns(self) -> list:
+        return self.numerical_columns
+
+    def categorical_columns(self) -> list:
+        return self.categorical_columns
+
+    def unique_values(self, columns: list = None) -> dict:
+        result = dict()
+        if columns:
+            # for a subset
+            for col in columns:
+                result[col] = self.df[col].nunique()
+        else:
+            # for entire dataframe
+            for col in self.df.columns:
+                result[col] = self.df[col].nunique()
+                
+        return result
+
+    def print_eda_report(self):
+        print(f"================= {self.dataset.label} =================")
+        print(f"{self.dataset.label} has shape {self.dataset.dataframe.shape}")
+        print()
+        print(f"{self.dataset.label} has numerical data in columns: {self.numerical_columns}")
+        numerical_dict = self.unique_values(columns=self.numerical_columns)
+        for key, value in numerical_dict.items():
+            print(f'- Column "{key}" has {value} unique values.')
+        print()
+        print(f"{self.dataset.label} has categorical data in columns: {self.categorical_columns}")
+        categorical_dict = self.unique_values(columns=self.categorical_columns)
+        for key, value in categorical_dict.items():
+            print(f'- Column "{key}" has {value} unique values.')
+        print()
+
